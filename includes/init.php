3611e024eab95e13d0764af7ef21e99c35a538af
Manny Perez <threedyou@gmail.com>
2017-02-25 10:19:56 -0600

    Constants for database credentials added to init.php

---
+++ includes/init.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
+defined('DB_USER') ? NULL : define('DB_USER', 'mperez');
+defined('DB_PASS') ? NULL : define('DB_PASS', 'admin0112@');
+defined('DB_NAME') ? NULL : define('DB_NAME', 'circuitTester');
+
+?>
